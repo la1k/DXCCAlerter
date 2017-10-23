c6346431f16fb9aac16d656f7c439fffcbc96452
Asgeir Bjørgan <asgeirbj@samfundet.no>
2017-10-23 18:46:27 +0200

    Rectify clublog URL

--- get_clublog_dxcc_matrix.sh
+++ get_clublog_dxcc_matrix.sh
@@ -14,4 +14,4 @@ callsign=$(get_config_option "callsign")
 email=$(get_config_option "clublog_email")
 password=$(get_config_option "clublog_password")
 output_filename=$(get_config_option "dxcc_matrix_filename")
-curl -s "https://secure.clublog.org/json_dxccchart.php?call=$callsign\&api=$API_key\&email=$email\&password=$password\&mode=0" > $output_filename
+curl -s "https://secure.clublog.org/json_dxccchart.php?call=$callsign&api=$API_key&email=$email&password=$password&mode=0" > $output_filename
